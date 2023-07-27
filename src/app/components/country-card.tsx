db96fb6a5c7b1c578102f20538244179c3eef3c7
Riku Putkinen <putkinenriku@gmail.com>
2023-07-27 22:56:14 +0300

    Add number formatting

--- src/app/components/country-card.tsx
+++ src/app/components/country-card.tsx
@@ -13,15 +13,20 @@ export default function CountryCard({ countryData } : { countryData : CountryDat
     area
   } = countryData;
 
+  const formatting = new Intl.NumberFormat("en-US", {
+    style: "decimal",
+    useGrouping: true,
+  });
+
   return (
     <li>
       <Link href={`/${name.official}`}>
         <Image src={flags.png} alt={flags.alt} height={200} width={200}/>
-        <h2>{name.official}</h2>
+        <h2 className="font-bold mt-1">{name.official}</h2>
         <p><span className="font-bold">Capital: </span>{capital}</p>
         <p><span className="font-bold">Region: </span>{region}</p>
-        <p><span className="font-bold">Area: </span>{area} km<sup>2</sup></p>
-        <p><span className="font-bold">Population: </span>{population}</p>
+        <p><span className="font-bold">Area: </span>{formatting.format(area)} km<sup>2</sup></p>
+        <p><span className="font-bold">Population: </span>{formatting.format(population)}</p>
       </Link>
     </li>
   )
