dad0007bd48556b65eca2b96c7aca9f8b253d87c
Riku Putkinen <putkinenriku@gmail.com>
2023-08-01 23:27:46 +0300

    Add a navbar

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import Link from "next/link";
+import { useTheme } from "next-themes";
+import { useState, useEffect } from 'react';
+
+export default function Navbar() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, [])
+
+  if (!mounted) {
+    return null;
+  }
+
+  function toggleTheme(theme: string | undefined) {
+    return theme === 'dark' ? setTheme('light') : setTheme('dark');
+  }
+
+  return (
+    <nav className="text-xl flex justify-between px-2 py-3 bg-gray-100 dark:bg-gray-900 shadow-md">
+      <Link href={'/'} className="hover:underline">Country App</Link>
+      <button onClick={() => toggleTheme(theme)}>{theme === 'dark' ? 'Switch to light theme' : 'Switch to dark theme'}</button>
+    </nav>
+  )
+}
